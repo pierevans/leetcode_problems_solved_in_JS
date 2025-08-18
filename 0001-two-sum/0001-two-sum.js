1d724ed39c4b6df5a2959edca4e7dff0d01bad16
pierevans <pier_evans94@yahoo.com>
2025-08-18 11:17:43 -0500

    Time: 50 ms (17.75%), Space: 56.1 MB (20.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,7 +6,6 @@
 var twoSum = function(nums, target) {
     let targetSumIndex = []
     for(let i = 0; i < nums.length; i++) {
-        let num = nums[i]
         for(let j = i+1; j < nums.length; j++)
         if(nums[i] + nums[j] === target) {
             targetSumIndex.push(i, j)
